fcc5a70e64648d90fe50267f97ead53375ab93b1
Jimish <jimish957@gmail.com>
2021-12-16 00:18:18 +0530

    fixed movements of assets

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 console.log("Jay shree ram");
+
+let movementField = document.getElementById("name_assets");
+
+movementField.addEventListener("mousemove",function(e)
+{
+    let x = e.clientX;
+    let y = e.clientY;
+    // console.log(x,y);
+
+    let asset2 = document.getElementById("asset2");
+    let asset3 = document.getElementById("asset3");
+    
+    asset2.style.transform = `translate(${x*0.02}px,${y*0.08}px)`;
+    asset3.style.transform = `translate(-${x*0.02}px,-${y*0.08}px)`;
+})
