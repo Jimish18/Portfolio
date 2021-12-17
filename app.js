3e5c5cf310d180c3fd9824eae03bfd9022682565
Jimish <jimish957@gmail.com>
2021-12-17 22:26:27 +0530

    set image background color and asset4 and it's movement

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 console.log("Jay shree ram");
 
-let movementField = document.getElementById("name_assets");
+let movementField1 = document.getElementById("name_assets");
 
-movementField.addEventListener("mousemove",function(e)
+movementField1.addEventListener("mousemove",function(e)
 {
     let x = e.clientX;
     let y = e.clientY;
@@ -14,4 +14,21 @@ movementField.addEventListener("mousemove",function(e)
     asset2.style.transform = `translate(${x*0.02}px,${y*0.08}px)`;
     asset3.style.transform = `translate(-${x*0.02}px,-${y*0.08}px)`;
     asset2.style.transformStyle = "preserve-3d";
-})
+    asset3.style.transformStyle = "preserve-3d";
+
+});
+
+let movementField2 = document.getElementById("image_intro");
+
+movementField2.addEventListener("mousemove",function(e)
+{
+    let x = e.clientX;
+    let y = e.clientY;
+    // console.log(x,y);
+
+    let asset4 = document.getElementById("asset4");
+    // // let asset5 = document.getElementById("asset5");
+
+    asset4.style.transform = `translate(${x*0.02}px,${y*0.02}px)`; 
+    asset4.style.transformStyle = "preserve-3d";
+});
